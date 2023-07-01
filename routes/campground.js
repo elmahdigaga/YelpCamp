f8e398cf9a2319cbd989c3bcb25ff4f9b8297b8d
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-07-01 21:23:29 +0100

    Added Campgrounds GET by id route

--- routes/campground.js
+++ routes/campground.js
@@ -18,4 +18,23 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ error: "Invalid Campground ID" });
+        }
+
+        const campground = await Campground.findOne({ _id: id });
+        if (!campground) {
+            return res.status(404).json({ error: "Campground Not Found" });
+        }
+
+        res.status(200).json({ campground });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
 module.exports = router;
