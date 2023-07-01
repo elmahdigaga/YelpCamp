9042eb104552bf1d5f3d7da61ed60b6f5333a3c1
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-07-01 20:11:19 +0100

    Added ejs view engine support

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
 const { connectDatabase } = require("./config/database");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 connectDatabase();
 
