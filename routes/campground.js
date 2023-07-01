3a40cdb5082c507404268c94dd81ac69eac57bc8
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-07-01 21:58:42 +0100

    Updated campgrounds routes to render views

--- routes/campground.js
+++ routes/campground.js
@@ -9,13 +9,17 @@ router.get("/", async (req, res) => {
         const campgrounds = await Campground.find();
 
         if (campgrounds.length === 0) {
-            res.status(404).json({ error: "No Campgrounds Found" });
+            res.status(404).render("errors/not-found", {
+                error: "No Campgrounds Found",
+            });
         }
 
-        res.status(200).json({ campgrounds });
+        res.status(200).render("campgrounds/all", { campgrounds });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
+        res.status(500).render("errors/internal-server", {
+            error: "Internal Server Error",
+        });
     }
 });
 
@@ -23,18 +27,24 @@ router.get("/:id", async (req, res) => {
     try {
         const { id } = req.params;
         if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(404).json({ error: "Invalid Campground ID" });
+            res.status(404).render("errors/not-found", {
+                error: "Campground Not Found",
+            });
         }
 
         const campground = await Campground.findOne({ _id: id });
         if (!campground) {
-            return res.status(404).json({ error: "Campground Not Found" });
+            res.status(404).render("errors/not-found", {
+                error: "Campground Not Found",
+            });
         }
 
         res.status(200).json({ campground });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
+        res.status(500).render("errors/internal-server", {
+            error: "Internal Server Error",
+        });
     }
 });
 
@@ -42,18 +52,24 @@ router.delete("/:id", async (req, res) => {
     try {
         const { id } = req.params;
         if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(404).json({ error: "Invalid Campground ID" });
+            res.status(404).render("errors/not-found", {
+                error: "Campground Not Found",
+            });
         }
 
         const campground = await Campground.findOneAndDelete({ _id: id });
         if (!campground) {
-            return res.status(404).json({ error: "Campground Not Found" });
+            res.status(404).render("errors/not-found", {
+                error: "Campground Not Found",
+            });
         }
 
         res.status(200).json({ campground });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
+        res.status(500).render("errors/internal-server", {
+            error: "Internal Server Error",
+        });
     }
 });
 
