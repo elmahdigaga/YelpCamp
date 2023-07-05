daa981b83c841e0b5c093e39d3a4c15d4ef2e694
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-07-05 14:30:48 +0100

    Added validation messages in Campground model

--- models/campground.js
+++ models/campground.js
@@ -4,22 +4,22 @@ const { Schema } = mongoose;
 const campgroundSchema = new Schema({
     name: {
         type: String,
-        required: [true, "Campground name is required"],
+        required: [true, "Name is required"],
     },
     image: {
         type: String,
-        required: [true, "Campground image is required"],
+        required: [true, "Image URL is required"],
     },
     price: {
         type: Number,
-        required: [true, "Campground price is required"],
+        required: [true, "Price is required"],
     },
     description: {
         type: String,
     },
     location: {
         type: String,
-        required: [true, "Campground location is required"],
+        required: [true, "Location is required"],
     },
     date: {
         type: Date,
