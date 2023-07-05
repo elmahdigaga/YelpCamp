9a48a54b734d0a8625bc9293c69d12653a3f736e
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-07-05 12:24:55 +0100

    Edit Campground Schema

--- models/campground.js
+++ models/campground.js
@@ -4,18 +4,22 @@ const { Schema } = mongoose;
 const campgroundSchema = new Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, "Campground name is required"],
+    },
+    image: {
+        type: String,
+        required: [true, "Campground image is required"],
     },
     price: {
         type: Number,
-        required: true,
+        required: [true, "Campground price is required"],
     },
     description: {
         type: String,
     },
     location: {
         type: String,
-        required: true,
+        required: [true, "Campground location is required"],
     },
     date: {
         type: Date,
