e8b5ecef41fc61d265a89d89f987ea183c417ab7
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-07-07 19:46:48 +0100

    Added Cascading delete functionality, to automaticaly delete campgrounds when the owner user is deleted

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
+const Campground = require("./campground");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
@@ -22,6 +23,20 @@ const userSchema = new Schema({
 
 // The plugin automatically adds the username, salt and hash fields in the schema, aswell as other methods
 userSchema.plugin(passportLocalMongoose);
+
+// Adding a post middleware to delete associated campgrounds when a user is deleted
+userSchema.post("findOneAndDelete", async function (user) {
+    try {
+        const campgrounds = await Campground.find({ author: user._id });
+        if (campgrounds.length > 0) {
+            for (let campground of campgrounds) {
+                await Campground.findOneAndDelete({ _id: campground._id });
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 const User = mongoose.model("User", userSchema);
 
