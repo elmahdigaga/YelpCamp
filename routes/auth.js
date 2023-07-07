4c820f68564d34f8a2edec2dde4ffd6c86cc7e06
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-07-07 13:25:23 +0100

    Added registration GET route

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,8 @@ const { handleErrors } = require("../utils/helpers");
 
 const router = express.Router();
 
+router.get("/register", (req, res) => {
+    res.status(200).render("/auth/register");
+});
+
 module.exports = router;
