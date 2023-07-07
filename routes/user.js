03930bee3e36e3d28ea1fba0881bf762df5f3fa8
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-07-07 13:21:32 +0100

    Created User router

---
+++ routes/user.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const User = require("../models/user");
+const { handleErrors } = require("../utils/helpers");
+
+const router = express.Router();
+
+module.exports = router;
