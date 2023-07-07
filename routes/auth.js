95feee70af41a04288a9bcb8ff6696a620e31ee0
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-07-07 14:35:58 +0100

    Added registration view

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,11 @@ const { handleErrors } = require("../utils/helpers");
 const router = express.Router();
 
 router.get("/register", (req, res) => {
-    res.status(200).render("/auth/register");
+    res.status(200).render("auth/register");
 });
 
 router.get("/login", (req, res) => {
-    res.status(200).render("/auth/login");
+    res.status(200).render("auth/login");
 });
 
 module.exports = router;
