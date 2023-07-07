c83991262f6e0fe1eb80cd92032d6358b3f43236
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-07-07 12:58:02 +0100

    Added passport middleware config in server file

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+require("dotenv").config();
 const campgroundsRouter = require("./routes/campground");
 const reviewsRouter = require("./routes/review");
 const { connectDatabase } = require("./config/database");
-require("dotenv").config();
+const User = require("./models/user");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Parsing middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+// Session middlewares
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
@@ -27,17 +33,24 @@ app.use(
     })
 );
 app.use(flash());
-app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
     res.locals.flashSuccess = req.flash("success");
     res.locals.flashError = req.flash("error");
     next();
 });
 
+// Auth middlewares
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // Configure server
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to the database
 connectDatabase();
