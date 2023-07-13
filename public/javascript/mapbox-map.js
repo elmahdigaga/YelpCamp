3c2f0e98dd46efce6a834b536a65b3e355ed5666
elmahdigaga <elmahdigaga9@gmail.com>
2023-07-13 01:04:51 +0100

    Added popup text for campground map marker Changed map style to: outdoors

--- public/javascript/mapbox-map.js
+++ public/javascript/mapbox-map.js
@@ -1,10 +1,17 @@
 mapboxgl.accessToken = mapboxToken;
 var map = new mapboxgl.Map({
     container: "map",
-    center: coordinates,
+    center: campground.geometry.coordinates,
     zoom: 10,
-    style: "mapbox://styles/mapbox/streets-v11",
+    style: "mapbox://styles/mapbox/outdoors-v12",
     hash: true,
 });
 
-const marker = new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+    `<h5>${campground.name}</h5>`
+);
+
+const marker = new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(popup)
+    .addTo(map);
