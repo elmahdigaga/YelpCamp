b24ff815b834cbc102f152d9ab6eb4e52ac04141
elmahdigaga <elmahdigaga9@gmail.com>
2023-07-14 15:22:31 +0100

    Added campground form HTML sanitizing to avoid XSS vulnerability

--- middlewares/validation/validate-campground.js
+++ middlewares/validation/validate-campground.js
@@ -1,4 +1,29 @@
-const Joi = require("joi");
+const baseJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+    type: "string",
+    base: joi.string(),
+    messages: {
+        "string.escapeHTML": "{{#label}} must not include HTML",
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) {
+                    return helpers.error("string.escapeHTML", { value });
+                }
+                return clean;
+            },
+        },
+    },
+});
+
+const Joi = baseJoi.extend(extension);
 
 const campgroundSchema = Joi.object({
     name: Joi.string().min(3).max(30).required(),
